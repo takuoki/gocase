af913ffbbbca58e0c5972ca884f6abae591f728a
takuo.oki <takuo.oki@optim.co.jp>
2022-09-18 14:33:06 +0900

    implement converter option

--- README.md
+++ README.md
@@ -1,26 +1,58 @@
 # gocase
 
+[![GoDoc](https://godoc.org/github.com/takuoki/gocase?status.svg)](https://godoc.org/github.com/takuoki/gocase)
 [![CircleCI](https://circleci.com/gh/takuoki/gocase/tree/master.svg?style=shield&circle-token=06a2582cde9cc3c182873c3ec5dddb67e9388cf6)](https://circleci.com/gh/takuoki/gocase/tree/master)
 [![codecov](https://codecov.io/gh/takuoki/gocase/branch/master/graph/badge.svg)](https://codecov.io/gh/takuoki/gocase)
-[![GoDoc](https://godoc.org/github.com/takuoki/gocase?status.svg)](https://godoc.org/github.com/takuoki/gocase)
 [![MIT License](http://img.shields.io/badge/license-MIT-blue.svg?style=flat)](LICENSE)
 
 A golang package to convert normal CamelCase to Golang's CamelCase and vice versa.
 Golang's CamelCase means a string that takes into account to Go's common initialisms.
 
+## Install
+
+```cmd
+go get -u github.com/takuoki/gocase
+```
+
+## Usage
+
+### Default converter
+
+```go
+str := gocase.To("IpAddress") // "IPAddress"
+```
+
+```go
+str := gocase.To("defaultDNSServer") // "defaultDnsServer"
+```
+
+### Custom converter
+
+```go
+converter, _ := gocase.New(gocase.WithInitialisms("JSON", "CSV"))
+
+str1 := converter.To("IpAddress") // "IpAddress" (no convert)
+str2 := converter.To("JsonFile")  // "JSONFile"
+str3 := converter.To("CsvFile")   // "CSVFile"
+```
+
 ## Example
 
-* `To`: converts a string from `From` to `To`.
-* `Revert`: converts a string from `To` to `From`.
+The default converter converts as follows
 
-| From | To |
-|:-|:-|
-|`""`|`""`|
-|`"jsonFile"`|`"jsonFile"`|
-|`"IpAddress"`|`"IPAddress"`|
-|`"defaultDnsServer"`|`"defaultDNSServer"`|
-|`"somethingHttpApiId"`|`"somethingHTTPAPIID"`|
+- `To`: converts a string from `From` to `To`.
+- `Revert`: converts a string from `To` to `From`.
+
+| From                 | To                   |
+| :------------------- | :------------------- |
+| ""                   | ""                   |
+| "jsonFile"           | "jsonFile"           |
+| "IpAddress"          | "IPAddress"          |
+| "defaultDnsServer"   | "defaultDNSServer"   |
+| "somethingHttpApiId" | "somethingHTTPAPIID" |
+| "somethingUuid"      | "somethingUUID"      |
+| "somethingSip"       | "somethingSIP"       |
 
 ## Reference
 
-For more details, please see [golint package](https://github.com/golang/lint/blob/d0100b6bd8b389f0385611eb39152c4d7c3a7905/lint.go#L768-L810).
+For more details, please see [initialisms section](https://staticcheck.io/docs/configuration/options/#initialisms) in [Staticcheck](https://staticcheck.io/).
--- gocase_test.go
+++ gocase_test.go
@@ -6,40 +6,58 @@ import (
 	"github.com/takuoki/gocase"
 )
 
-func TestTo(t *testing.T) {
+func TestConverter_To(t *testing.T) {
+	dc, _ := gocase.New()
+	cc, _ := gocase.New(gocase.WithInitialisms("JSON", "CSV"))
+
 	cases := []struct {
-		s, expected string
+		conv    *gocase.Converter
+		s, want string
 	}{
-		{s: "", expected: ""},
-		{s: "jsonFile", expected: "jsonFile"},
-		{s: "IpAddress", expected: "IPAddress"},
-		{s: "defaultDnsServer", expected: "defaultDNSServer"},
-		{s: "somethingHttpApiId", expected: "somethingHTTPAPIID"},
+		{conv: dc, s: "", want: ""},
+		{conv: dc, s: "jsonFile", want: "jsonFile"},
+		{conv: dc, s: "IpAddress", want: "IPAddress"},
+		{conv: dc, s: "defaultDnsServer", want: "defaultDNSServer"},
+		{conv: dc, s: "somethingHttpApiId", want: "somethingHTTPAPIID"},
+		{conv: dc, s: "somethingUuid", want: "somethingUUID"},
+		{conv: dc, s: "somethingSip", want: "somethingSIP"},
+		{conv: cc, s: "JsonFile", want: "JSONFile"},
+		{conv: cc, s: "CsvFile", want: "CSVFile"},
+		{conv: cc, s: "IpAddress", want: "IpAddress"},
 	}
 
 	for _, c := range cases {
-		r := gocase.To(c.s)
-		if r != c.expected {
-			t.Errorf("value doesn't match: %s (expected %s)", r, c.expected)
+		r := c.conv.To(c.s)
+		if r != c.want {
+			t.Errorf("value doesn't match: %s (want %s)", r, c.want)
 		}
 	}
 }
 
-func TestRevert(t *testing.T) {
+func TestConverter_Revert(t *testing.T) {
+	dc, _ := gocase.New()
+	cc, _ := gocase.New(gocase.WithInitialisms("JSON", "CSV"))
+
 	cases := []struct {
-		s, expected string
+		conv    *gocase.Converter
+		s, want string
 	}{
-		{s: "", expected: ""},
-		{s: "jsonFile", expected: "jsonFile"},
-		{s: "IPAddress", expected: "IpAddress"},
-		{s: "defaultDNSServer", expected: "defaultDnsServer"},
-		{s: "somethingHTTPAPIID", expected: "somethingHttpApiId"},
+		{conv: dc, s: "", want: ""},
+		{conv: dc, s: "jsonFile", want: "jsonFile"},
+		{conv: dc, s: "IPAddress", want: "IpAddress"},
+		{conv: dc, s: "defaultDNSServer", want: "defaultDnsServer"},
+		{conv: dc, s: "somethingHTTPAPIID", want: "somethingHttpApiId"},
+		{conv: dc, s: "somethingUUID", want: "somethingUuid"},
+		{conv: dc, s: "somethingSIP", want: "somethingSip"},
+		{conv: cc, s: "JSONFile", want: "JsonFile"},
+		{conv: cc, s: "CSVFile", want: "CsvFile"},
+		{conv: cc, s: "somethingSIP", want: "somethingSIP"},
 	}
 
 	for _, c := range cases {
-		r := gocase.Revert(c.s)
-		if r != c.expected {
-			t.Errorf("value doesn't match: %s (expected %s)", r, c.expected)
+		r := c.conv.Revert(c.s)
+		if r != c.want {
+			t.Errorf("value doesn't match: %s (want %s)", r, c.want)
 		}
 	}
 }
